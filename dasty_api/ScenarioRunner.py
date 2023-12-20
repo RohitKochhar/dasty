cac83a0ca22137bb81cddfb9f31ad4cc9d21c544
Rohit Singh <rkochhar@uwaterloo.ca>
2023-12-20 14:45:38 -0400

    Improved response_body_check; Added tests

--- dasty_api/ScenarioRunner.py
+++ dasty_api/ScenarioRunner.py
@@ -1,6 +1,10 @@
-from .dasty import YAMLScenario
-import pathlib
+# Imports ---------------------------------------------------------------------
+# Standard library imports
+from pathlib import Path
+# Local application imports
+from .YAMLScenario import YAMLScenario
 
+# ScenarioRunner class -----------------------------------------------------------
 class ScenarioRunner():
     def __init__(self, directory_name, **kwargs):
         self.get_directory(directory_name)
@@ -10,18 +14,16 @@ class ScenarioRunner():
         """
         Checks if the directory exists
         """
-        if not pathlib.Path(directory_name).exists():
+        if not Path(directory_name).exists():
             raise Exception(f"Directory {directory_name} does not exist")
         else:
-            self.directory = pathlib.Path(directory_name)
+            self.directory = Path(directory_name)
 
     def collect_scenarios(self):
         """
         Collects all the scenarios in the directory
         """
-        # Get each file name in the target directory
         scenario_filepaths = [str(path) for path in self.directory.glob("*.yaml")]
-        # Create a YAML scenario for each file
         self.scenarios = [YAMLScenario(filepath=filepath) for filepath in scenario_filepaths]
 
     def run(self):
