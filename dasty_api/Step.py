26dd222cb1a87c0ffbff48378281847e4ca0b1c3
Rohit Singh <rkochhar@uwaterloo.ca>
2023-12-23 21:45:32 -0400

    Add support for all HTTP methods

--- dasty_api/Step.py
+++ dasty_api/Step.py
@@ -7,11 +7,14 @@ from .utils import check_response_body_contains, replace_variables
 # Classes ---------------------------------------------------------------------
 class Step:
     def __init__(
+            # Required parameters
             self,
             name: str,
             method: str,
             url: str,
             expected_status_code: int,
+            # Optional parameters
+            headers: dict = None,
             response_includes: dict = None, 
             response_excludes: dict = None,
             request_body: dict = None, 
@@ -20,10 +23,13 @@ class Step:
         """
         Constructs a Step object from the given parameters.
         """
+        # Required parameters
         self.name = name
         self.method = method
         self.url = url
         self.expected_status_code = expected_status_code
+        # Optional parameters
+        self.headers = headers
         self.request_body = request_body
         self.response_includes = response_includes
         self.response_excludes = response_excludes
@@ -35,14 +41,28 @@ class Step:
         self.url = self.url.format(**variables).replace("$", "")
         self.request_body = {key: value.format(**variables).replace("$", "") for key, value in self.request_body.items()} if self.request_body is not None else None
   
-        if self.method == "GET":
-            response = requests.get(self.url)
-        elif self.method == "POST":
-            response = requests.post(self.url, json=self.request_body)
-        elif self.method == "DELETE":
-            response = requests.delete(self.url)
+        method_func = {
+            "GET": requests.get,
+            "POST": requests.post,
+            "PUT": requests.put,
+            "DELETE": requests.delete,
+            "PATCH": requests.patch,
+            "HEAD": requests.head,
+            "OPTIONS": requests.options,
+        }.get(self.method)
+
+        if not method_func:
+            raise ValueError(f"Unsupported HTTP method: {self.method}")
+
+        # Call the requests function with the provided parameters
+        if self.method in ["GET", "HEAD", "OPTIONS"]:
+            response = method_func(self.url, headers=self.headers)
+        elif self.method in ["POST", "PUT", "PATCH", "DELETE"]:
+            response = method_func(self.url, json=self.request_body, headers=self.headers)
         else:
-            raise ValueError(f"Invalid method {self.method}")
+            # NOTE: This block is unreachable, but is kept here for completeness
+            raise ValueError(f"Unsupported HTTP method: {self.method}")
+
         assert response.status_code == self.expected_status_code, f'Error during \"{self.name}\" step:\nExpected {self.expected_status_code}, instead got {response.status_code}'
         
         # Replace variables in response_includes and perform the check
@@ -51,6 +71,7 @@ class Step:
             response_json = response.json()
             assert check_response_body_contains(response_json, formatted_response_includes), f'Error during \"{self.name}\" step:\nResponse: \n{response_json}\n Does not contain: \n{formatted_response_includes}'
 
+        # Replace variables in response_excludes and perform the check
         if self.response_excludes is not None:
             formatted_response_excludes = replace_variables(self.response_excludes, variables)
             response_json = response.json()
