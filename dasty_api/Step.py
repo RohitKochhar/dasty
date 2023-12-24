529354a2926cc21c73e036e21e20888e144638d3
Rohit Singh <33257917+RohitKochhar@users.noreply.github.com>
2023-12-23 21:28:38 -0400

    Add `response_excludes` functionality (#9)

--- dasty_api/Step.py
+++ dasty_api/Step.py
@@ -6,13 +6,27 @@ from .utils import check_response_body_contains, replace_variables
 
 # Classes ---------------------------------------------------------------------
 class Step:
-    def __init__(self, name: str, method: str, url: str, expected_status_code: int, response_contains: dict = None, request_body: dict = None, extract: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            method: str,
+            url: str,
+            expected_status_code: int,
+            response_includes: dict = None, 
+            response_excludes: dict = None,
+            request_body: dict = None, 
+            extract: list = None
+        ) -> None:
+        """
+        Constructs a Step object from the given parameters.
+        """
         self.name = name
         self.method = method
         self.url = url
         self.expected_status_code = expected_status_code
         self.request_body = request_body
-        self.response_contains = response_contains
+        self.response_includes = response_includes
+        self.response_excludes = response_excludes
         self.extract = extract
 
     def __call__(self, variables) -> dict:
@@ -31,11 +45,16 @@ class Step:
             raise ValueError(f"Invalid method {self.method}")
         assert response.status_code == self.expected_status_code, f'Error during \"{self.name}\" step:\nExpected {self.expected_status_code}, instead got {response.status_code}'
         
-        # Replace variables in response_contains and perform the check
-        if self.response_contains is not None:
-            formatted_response_contains = replace_variables(self.response_contains, variables)
+        # Replace variables in response_includes and perform the check
+        if self.response_includes is not None:
+            formatted_response_includes = replace_variables(self.response_includes, variables)
             response_json = response.json()
-            assert check_response_body_contains(response_json, formatted_response_contains), f'Error during \"{self.name}\" step:\nResponse: \n{response_json}\n Does not contain: \n{formatted_response_contains}'
+            assert check_response_body_contains(response_json, formatted_response_includes), f'Error during \"{self.name}\" step:\nResponse: \n{response_json}\n Does not contain: \n{formatted_response_includes}'
+
+        if self.response_excludes is not None:
+            formatted_response_excludes = replace_variables(self.response_excludes, variables)
+            response_json = response.json()
+            assert not check_response_body_contains(response_json, formatted_response_excludes), f'Error during \"{self.name}\" step:\nResponse: \n{response_json}\n Contains: \n{formatted_response_excludes}'
 
         # Save response values into variables if specified
         if self.extract:
